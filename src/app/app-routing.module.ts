8fb17a6eda229ff028dce9e3251d6a786da7a355
theabdul97@gmail.com <theabdul97@gmail.com>
2023-02-01 00:05:41 +0530

    Wizard Project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactDetailsComponent } from './contact-details/contact-details.component';
+import { PersonalDetailsComponent } from './personal-details/personal-details.component';
+import { SkillsComponent } from './skills/skills.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { WizardFinishedComponent } from './wizard-finished/wizard-finished.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: "", redirectTo: "welcome", pathMatch: "full" },
+{path: "welcome", component: WelcomeComponent},
+{path:"step-1", component: PersonalDetailsComponent },
+{path:"step-2", component: ContactDetailsComponent },
+{path:"step-3", component: SkillsComponent },
+{path:"step-4", component: WorkExperienceComponent },
+{path:"wizard-finished", component: WizardFinishedComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/wizard-finished/wizard-finished.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wizard-finished',
+  templateUrl: './wizard-finished.component.html',
+  styleUrls: ['./wizard-finished.component.css']
+})
+export class WizardFinishedComponent {
+
+}
